8f078e2068878869e91e680b5ea5fafa86a80b99
ki-hi-ro <hiroki.hiroki@icloud.com>
2026-05-28 12:52:35 +0900

    記事を1件取得するAPI

--- app/main.py
+++ app/main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+posts = [
+    {
+        "id": 1,
+        "title": "職人的フリーランスへの第一歩",
+        "content": "FastAPIで自分のブログサービスを作り始めた。",
+        "tags": ["FastAPI", "Python", "Freelance"],
+        "work_time_minutes": 30,
+    },
+    {
+        "id": 2,
+        "title": "理想の働き方につながるものだけを積む",
+        "content": "市場に見える形で成果物を積み上げていく。",
+        "tags": ["Career", "Portfolio"],
+        "work_time_minutes": 45,
+    },
+]
+
 
 @app.get("/")
 def read_root():
@@ -10,19 +27,13 @@ def read_root():
 
 @app.get("/posts")
 def read_posts():
-    return [
-        {
-            "id": 1,
-            "title": "職人的フリーランスへの第一歩",
-            "content": "FastAPIで自分のブログサービスを作り始めた。",
-            "tags": ["FastAPI", "Python", "Freelance"],
-            "work_time_minutes": 30,
-        },
-        {
-            "id": 2,
-            "title": "理想の働き方につながるものだけを積む",
-            "content": "市場に見える形で成果物を積み上げていく。",
-            "tags": ["Career", "Portfolio"],
-            "work_time_minutes": 45,
-        },
-    ]
+    return posts
+
+
+@app.get("/posts/{post_id}")
+def read_post(post_id: int):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+
+    return {"error": "Post not found"}
